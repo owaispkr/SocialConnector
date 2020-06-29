e9208250d44962b69f066306971ba73aeeab3684
owais parkar <owaisparkar94@outlook.com>
2020-06-29 21:27:30 +1200

    Field Components

---
+++ client/src/components/common/InputGroup.js
@@ -0,0 +1,53 @@
+import React from "react";
+import classnames from "classnames";
+import PropTypes from "prop-types";
+
+const InputGroup = ({
+  name,
+  placeholder,
+  value,
+  error,
+  icon,
+  type,
+  onChange,
+  disabled,
+}) => {
+  return (
+    <div className="form-group">
+      <div className="input-group-prepend">
+        <span className="input-group-text">
+          <i className={icon}></i>
+        </span>
+      </div>
+      <input
+        type={type}
+        className={classnames("form-control form-control-sm", {
+          "is-invalid": error,
+        })}
+        placeholder={placeholder}
+        name={name}
+        value={value}
+        onChange={onChange}
+        disabled={disabled}
+      />
+      {error && <div className="invalid-feedback">{error}</div>}
+    </div>
+  );
+};
+
+InputGroup.propTypes = {
+  name: PropTypes.string.isRequired,
+  placeholder: PropTypes.string,
+  value: PropTypes.string.isRequired,
+  error: PropTypes.string,
+  icon: PropTypes.string,
+  type: PropTypes.string,
+  onChange: PropTypes.func,
+  disabled: PropTypes.string,
+};
+
+InputGroup.defaultProps = {
+  type: "text",
+};
+
+export default InputGroup;
---
+++ client/src/components/common/TextAreaFieldGroup.js
@@ -0,0 +1,42 @@
+import React from "react";
+import classnames from "classnames";
+import PropTypes from "prop-types";
+
+const TextAreaFieldGroup = ({
+  name,
+  placeholder,
+  value,
+  error,
+  info,
+  onChange,
+  disabled,
+}) => {
+  return (
+    <div className="form-group">
+      <textarea
+        className={classnames("form-control form-control-sm", {
+          "is-invalid": error,
+        })}
+        placeholder={placeholder}
+        name={name}
+        value={value}
+        onChange={onChange}
+        disabled={disabled}
+      />
+      {info && <small className="form-text text-muted">{info}</small>}
+      {error && <div className="invalid-feedback">{error}</div>}
+    </div>
+  );
+};
+
+TextAreaFieldGroup.propTypes = {
+  name: PropTypes.string.isRequired,
+  placeholder: PropTypes.string,
+  value: PropTypes.string.isRequired,
+  error: PropTypes.string,
+  info: PropTypes.string,
+  onChange: PropTypes.func,
+  disabled: PropTypes.string,
+};
+
+export default TextAreaFieldGroup;
